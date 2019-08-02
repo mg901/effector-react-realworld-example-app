8ddf6b5135e3dc5641c7ae5c69a6c36336e13218
mg901 <mgreen097@gmail.com>
2019-08-02 19:01:05 +0300

    chore: add console.log

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-export const InputFiled = ({ type = 'text', value, placeholder, onChange }) => (
-  <fieldset className="form-group">
-    <input
-      className="form-control form-control-lg"
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  </fieldset>
-);
+export const InputFiled = ({ type = 'text', value, placeholder, onChange }) => {
+  console.log('type', type);
+
+  return (
+    <fieldset className="form-group">
+      <input
+        className="form-control form-control-lg"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </fieldset>
+  );
+};
