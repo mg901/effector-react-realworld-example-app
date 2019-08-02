bd0625809f1fbd435446d93ac856cb593ff9d4bb
mg901 <mgreen097@gmail.com>
2019-08-03 02:31:50 +0300

    refactor: input-field

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-export const InputFiled = ({ type = 'text', value, placeholder, onChange }) => {
-  console.log('type', type);
-
-  return (
-    <fieldset className="form-group">
-      <input
-        className="form-control form-control-lg"
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </fieldset>
-  );
-};
+export const InputFiled = ({ type = 'text', value, placeholder, onChange }) => (
+  <fieldset className="form-group">
+    <input
+      className="form-control form-control-lg"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  </fieldset>
+);
