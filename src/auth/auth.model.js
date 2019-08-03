71a15f140c8c877eee2c57b944ec88260fb6ccaf
mg901 <mgreen097@gmail.com>
2019-08-03 04:17:57 +0300

    refactor: replace model with auth.model

--- src/auth/auth.model.js
+++ src/auth/auth.model.js
@@ -1,23 +1,24 @@
 import { restore, createEvent, createEffect, merge, combine } from 'effector';
 import { auth } from '../agent';
 
-const eventValue = (e) => e.currentTarget.value;
+const targetValue = (e) => e.currentTarget.value;
 
 export const changeEmail = createEvent();
 export const changeUserName = createEvent();
 export const changePassword = createEvent();
 export const asyncSignIn = createEffect();
 export const asyncSignUp = createEffect();
-export const noAuth = merge([asyncSignIn.fail, asyncSignUp.fail]);
+export const authDone = merge([asyncSignIn.done, asyncSignUp.done]);
+export const authFail = merge([asyncSignIn.fail, asyncSignUp.fail]);
+
+export const onChangeUserName = changeUserName.prepend(targetValue);
+export const onChangeEmail = changeEmail.prepend(targetValue);
+export const onChangePassword = changePassword.prepend(targetValue);
 
 export const $username = restore(changeUserName, '');
 export const $email = restore(changeEmail, '');
 export const $password = restore(changePassword, '');
 export const $fields = combine([$username, $email, $password]);
-
-export const onChangeUserName = changeUserName.prepend(eventValue);
-export const onChangeEmail = changeEmail.prepend(eventValue);
-export const onChangePassword = changePassword.prepend(eventValue);
 
 asyncSignIn.use(({ email, password }) => auth.signIn(email, password));
 
