98abe6d2fb9c88639aa979621b810cd3ff2f018a
mg901 <mgreen097@gmail.com>
2019-08-03 03:14:31 +0300

    chore: add token saving

--- src/app/model.js
+++ src/app/model.js
@@ -1,14 +1,21 @@
 import { merge, createStore } from 'effector';
 import { noAuth, asyncSignIn, asyncSignUp } from '../auth/model';
+import { TOKEN_NAME } from '../constants';
+
+export const auth = merge([asyncSignIn.done, asyncSignUp.done]);
 
 export const $appName = createStore('Conduit');
 export const $currentUser = createStore(null);
 export const $viewChangeCounter = createStore(0);
-export const auth = merge([asyncSignIn.done, asyncSignUp.done]);
-
 export const $errors = createStore({});
 export const $token = $currentUser.map((user) => user && user.token);
 
 $currentUser.on(auth, (_, { result }) => result.user);
 
 $errors.on(noAuth, (_, { error }) => JSON.parse(error.response.text).errors);
+
+$token.watch((token) => {
+  if (token) {
+    localStorage.setItem(TOKEN_NAME, token);
+  }
+});
