d0e524363e1b7e9e8d8108fd280c68c9eb45ed2d
mg901 <mgreen097@gmail.com>
2019-08-05 13:53:42 +0300

    chore: add a api module

---
+++ src/api.js
@@ -0,0 +1,91 @@
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
+import { TOKEN_NAME } from './constants';
+
+const API_ROOT = 'https://conduit.productionready.io/api';
+const superagent = superagentPromise(_superagent, Promise);
+
+const tokenPlugin = (req) => {
+  const token = localStorage.getItem(TOKEN_NAME);
+
+  if (token) {
+    req.set('authorization', `Token ${token}`);
+  }
+};
+
+const responseBody = (res) => res.body;
+
+export const requests = {
+  get: (url) =>
+    superagent
+      .get(`${API_ROOT}${url}`)
+      .use(tokenPlugin)
+      .then(responseBody),
+  post: (url, body) =>
+    superagent
+      .post(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody),
+  put: (url, body) =>
+    superagent
+      .put(`${API_ROOT}${url}`, body)
+      .use(tokenPlugin)
+      .then(responseBody),
+  del: (url) =>
+    superagent
+      .del(`${API_ROOT}${url}`)
+      .use(tokenPlugin)
+      .then(responseBody),
+};
+
+export const auth = {
+  current: () => requests.get('/user'),
+  signIn: (email, password) =>
+    requests.post('/users/login', { user: { email, password } }),
+  signUp: (username, email, password) =>
+    requests.post('/users', { user: { username, email, password } }),
+  save: (user) => requests.put('/user', { user }),
+};
+
+export const tags = {
+  getAll: () => requests.get('/tags'),
+};
+
+const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
+const omitSlug = (article) => ({ ...article, slug: undefined });
+
+export const articles = {
+  all: (page) => requests.get(`/articles?${limit(10, page)}`),
+  byAuthor: (author, page) =>
+    requests.get(
+      `/articles?author=${encodeURIComponent(author)}&${limit(5, page)}`,
+    ),
+  byTag: (tag, page) =>
+    requests.get(`/articles?tag=${encodeURIComponent(tag)}&${limit(10, page)}`),
+  del: (slug) => requests.del(`/articles/${slug}`),
+  favorite: (slug) => requests.post(`/articles/${slug}/favorite`),
+  favoritedBy: (author, page) =>
+    requests.get(
+      `/articles?favorited=${encodeURIComponent(author)}&${limit(5, page)}`,
+    ),
+  feed: () => requests.get('/articles/feed?limit=10&offset=0'),
+  get: (slug) => requests.get(`/articles/${slug}`),
+  unfavorite: (slug) => requests.del(`/articles/${slug}/favorite`),
+  update: (article) =>
+    requests.put(`/articles/${article.slug}`, { article: omitSlug(article) }),
+  create: (article) => requests.post('/articles', { article }),
+};
+
+export const comments = {
+  forArticle: (slug) => requests.get(`/articles/${slug}/comments`),
+  create: (slug, comment) =>
+    requests.post(`/articles/${slug}/comments`, { comment }),
+  delete: (slug, commentId) =>
+    requests.del(`/articles/${slug}/comments/${commentId}`),
+};
+
+export const profile = {
+  get: (username) => requests.get(`/profiles/${username}`),
+  follow: (username) => requests.post(`/profiles/${username}/follow`),
+  unfollow: (username) => requests.del(`/profiles/${username}/follow`),
+};
