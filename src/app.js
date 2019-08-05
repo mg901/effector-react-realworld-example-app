cf141ce31b14d7afb63341218759c7a6f82a2c08
mg901 <mgreen097@gmail.com>
2019-08-05 13:54:12 +0300

    chore: add a "app" component

--- src/app.js
+++ src/app.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
-import { Header } from './components/header';
+import { useStore } from 'effector-react';
 
-import Article from './components/Article';
-import Editor from './components/Editor';
+// import Article from './components/Article';
+// import Editor from './components/Editor';
 import { Home } from './home';
+import { Header } from './header';
 import { SignIn } from './auth/sign-in';
 import { SignUp } from './auth/sign-up';
-import { Profile } from './components/Profile';
-import ProfileFavorites from './components/ProfileFavorites';
-import Settings from './settings';
+import { Settings } from './settings';
+// import { Profile } from './components/Profile';
+// import ProfileFavorites from './components/ProfileFavorites';
 
-export const App = () => (
-  <>
+import { asyncGetUser } from './auth/model';
+
+export const App = () => {
+  const isLoading = useStore(asyncGetUser.pending);
+
+  useEffect(() => {
+    asyncGetUser();
+  }, []);
+
+  return isLoading ? (
     <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={SignIn} />
-      <Route path="/register" component={SignUp} />
-      {/* <Route path="/editor/:slug" component={Editor} />
-      <Route path="/editor" component={Editor} />
-      <Route path="/article/:id" component={Article} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/@:username/favorites" component={ProfileFavorites} />
-      <Route path="/@:username" component={Profile} />  */}
-    </Switch>
-  </>
-);
+  ) : (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/register" component={SignUp} />
+        <Route path="/settings" component={Settings} />
+        {/* <Route path="/@:username" component={Profile} /> */}
+        {/* <Route path="/editor/:slug" component={Editor} />
+        <Route path="/editor" component={Editor} />
+        <Route path="/article/:id" component={Article} />
+        <Route path="/@:username/favorites" component={ProfileFavorites} /> */}
+      </Switch>
+    </>
+  );
+};
