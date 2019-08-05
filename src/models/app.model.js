65dcf738654f21ceb79f76e544fae3e016ebf4fb
mg901 <mgreen097@gmail.com>
2019-08-05 13:53:15 +0300

    chore: add a models folder

---
+++ src/models/app.model.js
@@ -0,0 +1,8 @@
+import { createStore } from 'effector';
+import { authFail } from '../auth/model';
+
+export const $appName = createStore('Conduit');
+export const $viewChangeCounter = createStore(0);
+export const $errors = createStore({});
+
+$errors.on(authFail, (_, { error }) => JSON.parse(error.response.text).errors);
---
+++ src/models/user.model.js
@@ -0,0 +1,88 @@
+import {
+  createEvent,
+  createEffect,
+  createStore,
+  forward,
+  restore,
+  combine,
+} from 'effector';
+import { TOKEN_NAME } from '../constants';
+import { authDone } from '../auth/model';
+
+import * as api from '../api';
+import { targetValue } from '../helpers';
+import { history } from './router.model';
+
+export const logOut = createEvent();
+const changeImageUrl = createEvent();
+const changeName = createEvent();
+const changeBio = createEvent();
+const changeEmail = createEvent();
+const changePassword = createEvent();
+
+export const asyncUpdateUserData = createEffect();
+
+export const onCahangeImageUrl = changeImageUrl.prepend(targetValue);
+export const onChangeName = changeName.prepend(targetValue);
+export const onChangeBio = changeBio.prepend(targetValue);
+export const onChangeEmail = changeEmail.prepend(targetValue);
+export const onChangePassword = changePassword.prepend(targetValue);
+
+export const $currentUser = createStore({});
+export const $token = $currentUser.map(({ token }) => token || null);
+export const $image = $currentUser.map(({ image }) => image || '');
+export const $username = $currentUser.map(({ username }) => username);
+export const $bio = $currentUser.map(({ bio }) => bio || '');
+export const $email = $currentUser.map(({ email }) => email);
+export const $password = restore(changePassword, '');
+
+$currentUser.on(authDone, (_, { result }) => result.user).reset(logOut);
+
+$token.watch((token) => {
+  if (token) {
+    localStorage.setItem(TOKEN_NAME, token);
+  }
+});
+
+logOut.watch(() => {
+  localStorage.removeItem(TOKEN_NAME);
+  history.push('/');
+});
+
+forward({
+  from: changeImageUrl,
+  to: $image,
+});
+
+forward({
+  from: changeName,
+  to: $username,
+});
+
+forward({
+  from: changeBio,
+  to: $bio,
+});
+
+forward({
+  from: changeEmail,
+  to: $email,
+});
+
+export const $user = combine(
+  $image,
+  $username,
+  $bio,
+  $email,
+  $password,
+  (image, username, bio, email, password) =>
+    password
+      ? { image, username, bio, email, password }
+      : { image, username, bio, email },
+);
+
+asyncUpdateUserData.use((user) => api.auth.save(user));
+
+asyncUpdateUserData.done.watch(() => {
+  history.push('/');
+});
