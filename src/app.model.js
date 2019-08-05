a2abbbd2d8aacf1f35d195706d80b967ecde26e3
mg901 <mgreen097@gmail.com>
2019-08-05 13:51:13 +0300

    refactor: remove legacy

--- src/app.model.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'effector';
-import { authFail } from './auth/auth.model';
-
-export const $appName = createStore('Conduit');
-export const $viewChangeCounter = createStore(0);
-export const $errors = createStore({});
-
-$errors.on(authFail, (_, { error }) => JSON.parse(error.response.text).errors);
--- src/settings/store.js
+++
@@ -1,6 +0,0 @@
-import { createEffect } from 'effector';
-import { auth } from '../agent';
-
-export const asyncUpdateUserData = createEffect();
-
-asyncUpdateUserData.use((user) => auth.save(user));
