3a2f7b57a14c57e5a844d2ea7b1cdc3f9d37ce6d
mg901 <mgreen097@gmail.com>
2019-08-06 09:03:55 +0300

    refactor: add reset for errors

--- src/models/app.model.js
+++ src/models/app.model.js
@@ -1,8 +1,10 @@
 import { createStore } from 'effector';
-import { authFail } from '../auth/model';
+import { authFail, authDone } from '../auth/model';
 
 export const $appName = createStore('Conduit');
 export const $viewChangeCounter = createStore(0);
 export const $errors = createStore({});
 
-$errors.on(authFail, (_, { error }) => JSON.parse(error.response.text).errors);
+$errors
+  .on(authFail, (_, { error }) => JSON.parse(error.response.text).errors)
+  .reset(authDone);
