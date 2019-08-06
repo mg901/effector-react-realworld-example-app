bb0350166fc2fea7bbdb7d0e846c5307f4e06e20
mg901 <mgreen097@gmail.com>
2019-08-06 09:03:30 +0300

    chore: add editor page

---
+++ src/editor/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ListErrors } from '../list-errors';
+import { Form } from './form';
+
+export const Editor = () => (
+  <div className="editor-page">
+    <div className="container page">
+      <div className="row">
+        <div className="col-md-10 offset-md-1 col-xs-12">
+          <ListErrors />
+          <Form />
+        </div>
+      </div>
+    </div>
+  </div>
+);
