d4b72d11adac91186d9cb771d2f334ff5f87e86c
mg901 <mgreen097@gmail.com>
2019-08-07 02:19:59 +0300

    refactor: remove edit-settings component

--- src/profile/edit-settings.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export const EditSettings = () => (
-  <Link to="/settings" className="btn btn-sm btn-outline-secondary action-btn">
-    <i className="ion-gear-a" /> Edit Profile Settings
-  </Link>
-);
