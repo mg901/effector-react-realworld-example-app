c218475133f5f4a3b22673343c0602782c84bf4a
mg901 <mgreen097@gmail.com>
2019-08-06 08:58:11 +0300

    refactor(button): add a "disabled" prop

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,18 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 
-export const Button = ({ type = 'button', className, onClick, children }) => (
-  <button type={type} className={`btn ${className}`} onClick={onClick}>
+export const Button = ({
+  type = 'button',
+  className,
+  onClick,
+  disabled = false,
+  children,
+}) => (
+  <button
+    type={type}
+    className={`btn ${className}`}
+    onClick={onClick}
+    disabled={disabled}>
     {children}
   </button>
 );
