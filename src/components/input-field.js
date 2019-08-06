cbf5ac14750effcb765e122e0b37f5b042569828
mg901 <mgreen097@gmail.com>
2019-08-06 08:58:35 +0300

    refactor(inputField): add a "name" field

--- src/components/input-field.js
+++ src/components/input-field.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-export const InputFiled = ({ type = 'text', value, placeholder, onChange }) => (
+export const InputFiled = ({
+  type = 'text',
+  value,
+  placeholder,
+  name = '',
+  onChange,
+}) => (
   <fieldset className="form-group">
     <input
       className="form-control form-control-lg"
       type={type}
+      name={name}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
