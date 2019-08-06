f020b9eff449f78f7a2ccc649e07f1b0b99d8be4
mg901 <mgreen097@gmail.com>
2019-08-07 02:19:04 +0300

    chroe: add useEffect hook

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ListErrors } from '../list-errors';
 import { Form } from './form';
+import { leavePage } from './model';
 
-export const Editor = () => (
-  <div className="editor-page">
-    <div className="container page">
-      <div className="row">
-        <div className="col-md-10 offset-md-1 col-xs-12">
-          <ListErrors />
-          <Form />
+export const Editor = () => {
+  useEffect(() => {
+    return () => {
+      leavePage();
+    };
+  });
+
+  return (
+    <div className="editor-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-10 offset-md-1 col-xs-12">
+            <ListErrors />
+            <Form />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
