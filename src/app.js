225458b14d92163abb1d40b0e25e45c93a003142
mg901 <mgreen097@gmail.com>
2019-08-06 08:59:28 +0300

    refactor: add "editor" and "settings"

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import { useStore } from 'effector-react';
 
-// import Article from './components/Article';
-// import Editor from './components/Editor';
 import { Home } from './home';
 import { Header } from './header';
 import { SignIn } from './auth/sign-in';
 import { SignUp } from './auth/sign-up';
+import { Editor } from './editor';
 import { Settings } from './settings';
-// import { Profile } from './components/Profile';
-// import ProfileFavorites from './components/ProfileFavorites';
+import { Profile } from './profile';
 
 import { asyncGetUser } from './auth/model';
 
@@ -30,10 +28,11 @@ export const App = () => {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={SignIn} />
         <Route path="/register" component={SignUp} />
-        <Route path="/settings" component={Settings} />
-        {/* <Route path="/@:username" component={Profile} /> */}
-        {/* <Route path="/editor/:slug" component={Editor} />
         <Route path="/editor" component={Editor} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/@:username" component={Profile} />
+        {/* <Route path="/@:username" component={Profile} /> 
+        <Route path="/editor/:slug" component={Editor} />
         <Route path="/article/:id" component={Article} />
         <Route path="/@:username/favorites" component={ProfileFavorites} /> */}
       </Switch>
