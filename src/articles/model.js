481bc434799f39eae1790dadce63b1328d6efeeb
mg901 <mgreen097@gmail.com>
2019-08-09 18:42:46 +0300

    chore(atricles): add model

---
+++ src/articles/model.js
@@ -0,0 +1,31 @@
+import { createEvent, createEffect, createStore, merge } from 'effector';
+import { del, post, get } from '../request';
+import { $token } from '../models/user';
+
+const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
+
+export const changeTab = createEvent();
+
+export const getArticles = createEffect().use((page) =>
+  get(`/articles?${limit(10, page)}`),
+);
+
+export const getFeed = createEffect().use(() =>
+  get('/articles/feed?limit=10&offset=0'),
+);
+
+export const setFavoriteArticle = createEffect().use((slug) =>
+  post(`/articles/${slug}/favorite`),
+);
+export const setUnfavoriteArticle = createEffect().use((slug) =>
+  del(`/articles/${slug}/favorite`),
+);
+
+export const $articles = createStore({});
+
+$articles.on(merge([getArticles.done, getFeed.done]), (store, { result }) => ({
+  ...store,
+  ...result,
+}));
+
+$token.watch((token) => (token ? getFeed() : getArticles()));
