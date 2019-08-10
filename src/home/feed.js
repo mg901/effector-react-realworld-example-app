42473687a0803ab0337cd6a64e8c139a6e467308
mg901 <mgreen097@gmail.com>
2019-08-10 17:48:13 +0300

    chore: add acritles store

--- src/home/feed.js
+++ src/home/feed.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useStore } from 'effector-react';
+import { $articles } from '../articles/model';
 
-export const Feed = () => (
-  <div className="col-md-9">
-    <div className="feed-toggle">
-      <ul className="nav nav-pills outline-active" />
+export const Feed = () => {
+  const acticles = useStore($articles);
+
+  return (
+    <div className="col-md-9">
+      <div className="feed-toggle">
+        <ul className="nav nav-pills outline-active" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
