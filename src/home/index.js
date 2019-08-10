b09991b91b8cc74e6748811cb8f55738cce3501d
mg901 <mgreen097@gmail.com>
2019-08-10 15:34:34 +0300

    refactor: remove unused vars

--- src/home/index.js
+++ src/home/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
-
-import { useStore } from 'effector-react';
 import { Banner } from './banner';
 import { Feed } from './feed';
-import { $articles } from '../articles/model';
 
-export const Home = () => {
-  return (
-    <div className="home-page">
-      <Banner />
-      <div className="container page">
-        <div className="row">
-          <Feed />
-          <div className="col-md-3">
-            <div className="sidebar">
-              <p>Popular Tags</p>
-            </div>
+export const Home = () => (
+  <div className="home-page">
+    <Banner />
+    <div className="container page">
+      <div className="row">
+        <Feed />
+        <div className="col-md-3">
+          <div className="sidebar">
+            <p>Popular Tags</p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
