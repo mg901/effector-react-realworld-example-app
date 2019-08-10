5a4a0201501561f96617c61d86ca4e99c411a1a2
mg901 <mgreen097@gmail.com>
2019-08-10 15:32:06 +0300

    chore: add list-errors component

---
+++ src/components/list-errors.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ListErrors = ({ errors = {} }) => {
+  const listErrors = Object.keys(errors);
+
+  return (
+    listErrors.length > 0 && (
+      <ul className="error-messages">
+        {listErrors.map((key) => (
+          <li key={key}>
+            {key} {errors[key]}
+          </li>
+        ))}
+      </ul>
+    )
+  );
+};
