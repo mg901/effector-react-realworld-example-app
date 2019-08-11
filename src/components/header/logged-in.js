0f257ffd2e62db26b16541d5a2fe7ff46c944778
mg901 <mgreen097@gmail.com>
2019-08-11 23:29:41 +0300

    chore: add header

---
+++ src/components/header/logged-in.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useStore } from 'effector-react';
+import { NavLink } from 'react-router-dom';
+import { UserBar } from './user-bar';
+import { $authorizedUser } from '../../auth/model';
+
+export const LoggedIn = () => {
+  const { username, image } = useStore($authorizedUser);
+
+  return (
+    <>
+      <NavLink to="/" className="header-nav-link">
+        Home
+      </NavLink>
+
+      <NavLink to="/editor" className="header-nav-link">
+        New Post
+      </NavLink>
+
+      <UserBar image={image} username={username} />
+    </>
+  );
+};
