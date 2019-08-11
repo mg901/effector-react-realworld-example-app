9d62ce5d8afb9bc92b5d77fef6deb64fe456b7f3
mg901 <mgreen097@gmail.com>
2019-08-11 23:54:17 +0300

    style: add styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import { history } from './models/router';
 import { App } from './app';
+import 'normalize.css';
+import './styles/global.css';
+import './styles/layout.css';
+import './styles/typography.css';
 
 ReactDOM.render(
   <Router history={history}>
