d91e5468e139308c6c06974446e9200d1bed9b8f
mg901 <mgreen097@gmail.com>
2019-08-11 23:27:34 +0300

    chore: add settings

---
+++ src/settings/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ListErrors } from '../components/list-errors';
+import { Button } from '../components/button';
+import { Form } from './form';
+import { logOut } from '../auth/model';
+
+export const Settings = () => {
+  return (
+    <div className="container">
+      <h1 className="h1">Your Settings</h1>
+      <ListErrors />
+      <Form />
+      <hr />
+
+      <Button className="btn-outline-danger" onClick={() => logOut()}>
+        Or click here to logout.
+      </Button>
+    </div>
+  );
+};
