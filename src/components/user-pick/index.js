c63a7ec4f5a8011d57fd7591833c9d8d303fd122
mg901 <mgreen097@gmail.com>
2019-08-11 23:29:26 +0300

    chore: add user-pick

---
+++ src/components/user-pick/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from '../button';
+import './index.css';
+
+export const UserPick = ({ image, username, onClick }) => (
+  <Button className="user-pick-wrap" onClick={onClick}>
+    <img src={image} className="user-pick" alt={username} />
+  </Button>
+);
