89f2a1ac96982a83fad426c1ef89191dc41429c5
mg901 <mgreen097@gmail.com>
2019-08-11 23:30:02 +0300

    chore: add dorpdown-menu

---
+++ src/components/dropdown-menu/index.js
@@ -0,0 +1,29 @@
+import React, { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
+import cx from 'classnames';
+import { Button } from '../button';
+import './index.css';
+
+export const DownDownMenu = forwardRef(
+  ({ shown, username, onClick, onLogOutClick }, ref) => (
+    <ul
+      ref={ref}
+      className={cx('dropdown-menu', { 'dropdown-menu-active': shown })}>
+      <li className="dropdown-menu-item">
+        <Link to={`/@${username}`} className="link" onClick={onClick}>
+          Signed in as <span className="username">{username}</span>
+        </Link>
+      </li>
+      <li className="dropdown-menu-item">
+        <Link to="/settings" className="link" onClick={onClick}>
+          Settings
+        </Link>
+      </li>
+      <li className="dropdown-menu-item">
+        <Button className="link" onClick={onLogOutClick}>
+          logout
+        </Button>
+      </li>
+    </ul>
+  ),
+);
