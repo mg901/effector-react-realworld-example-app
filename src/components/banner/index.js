8c4b5cee25f0f39c99efd26055f66349863fdc0b
mg901 <mgreen097@gmail.com>
2019-08-12 00:07:18 +0300

    refactor: remove legacy markup

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,10 +3,8 @@ import { APP_NAME } from '../../constants';
 import './index.css';
 
 export const Banner = () => (
-  <div className="banner">
-    <div className="container">
-      <h1 className="logo banner-title">{APP_NAME.toLowerCase()}</h1>
-      <p>A place to share your knowledge.</p>
-    </div>
+  <div className="container banner">
+    <h1 className="logo banner-title">{APP_NAME.toLowerCase()}</h1>
+    <p>A place to share your knowledge.</p>
   </div>
 );
