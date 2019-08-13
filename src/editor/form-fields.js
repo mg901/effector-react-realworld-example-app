75a99d493b2fc7d24d47fad8559ff1e4d148a3d1
mg901 <mgreen097@gmail.com>
2019-08-14 01:09:17 +0300

    refactor: remove "input" component

--- src/editor/form-fields.js
+++ src/editor/form-fields.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { useFormField } from '../hooks';
-import { InputField } from '../components/input-field';
 import { TextField } from '../components/text-field';
 import { editor, onChangeText } from './model';
-
-export const Input = ({ name, placeholder, onKeyDown }) => {
-  const value = useFormField({ store: editor, name });
-
-  return (
-    <InputField
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChangeText}
-      onKeyDown={onKeyDown}
-    />
-  );
-};
 
 export const Textarea = ({ name, placeholder }) => {
   const value = useFormField({ store: editor, name });
