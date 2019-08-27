06fefccf2ee5c40c439109d8d7de81a7baff8e99
mg901 <mgreen097@gmail.com>
2019-08-27 20:35:01 +0300

    chore: add selectTag event & side effect for it

--- src/tags/model.js
+++ src/tags/model.js
@@ -1,9 +1,17 @@
-import { createEffect, createStore } from 'effector';
+import { createEffect, createStore, restore, createEvent } from 'effector';
 import { get } from '../request';
+import { history } from '../router';
 
+export const selectTag = createEvent();
 export const getTags = createEffect().use(() => get('/tags'));
+
+export const $selectedTag = restore(selectTag, null);
 
 export const $tags = createStore([]).on(
   getTags.done,
   (_, { result }) => result.tags,
 );
+
+selectTag.watch((tag) => {
+  history.push(`/feed/tags/${tag}`);
+});
