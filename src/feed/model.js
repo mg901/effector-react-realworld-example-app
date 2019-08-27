4d4e20b818099cee0229a98b4a8291b90e745cde
mg901 <mgreen097@gmail.com>
2019-08-27 20:38:11 +0300

    chore: add logic for posts by tag

--- src/feed/model.js
+++ src/feed/model.js
@@ -1,5 +1,17 @@
-import { getGlobalFeed, getUserFeed, getFeedByTag } from './model.events';
-import { $getGlobalFeed, $getUserFeed, $feedByTag } from './model.store';
+import { sample, createStore, combine, forward } from 'effector';
+import {
+  getGlobalFeed,
+  getUserFeed,
+  getFeedByTag,
+  refreshTimeout,
+} from './model.events';
+import { $selectedTag, selectTag } from '../tags/model';
+
+const initialState = { articles: [], articlesCount: null };
+
+export const $getGlobalFeed = createStore(initialState);
+export const $getUserFeed = createStore(initialState);
+export const $postsByTag = createStore({});
 
 $getGlobalFeed.on(getGlobalFeed.done, (state, { result }) => ({
   ...state,
@@ -11,7 +23,38 @@ $getUserFeed.on(getUserFeed.done, (state, { result }) => ({
   ...result,
 }));
 
-$feedByTag.on(getFeedByTag.done, (state, { result }) => ({
+$postsByTag.on(getFeedByTag.done, (state, { params, result }) => ({
   ...state,
-  ...result,
+  [params]: {
+    ...result,
+  },
 }));
+
+forward({
+  from: selectTag,
+  to: getFeedByTag,
+});
+
+/**
+  ON refreshTimeout.done call refreshTimeout again
+*/
+forward({
+  from: refreshTimeout.done,
+  to: refreshTimeout,
+});
+
+sample({
+  source: $selectedTag,
+  clock: refreshTimeout.done,
+  target: getFeedByTag,
+});
+
+export const $feedByTag = combine(
+  $postsByTag,
+  $selectedTag,
+  (postsMap, tag) => {
+    const posts = postsMap[tag];
+
+    return posts ? posts.articles : [];
+  },
+);
