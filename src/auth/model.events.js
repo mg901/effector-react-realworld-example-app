c9fd2d8084dfe3b2b104f0cc3fef91dc9d9ac367
mg901 <mgreen097@gmail.com>
2019-08-28 11:43:40 +0300

    chore: add setToken, getToken, removeToken

--- src/auth/model.events.js
+++ src/auth/model.events.js
@@ -1,10 +1,26 @@
 import { createEvent, createEffect, merge } from 'effector';
+import { TOKEN_NAME } from '../constants';
 import { get, post } from '../request';
+
+import { getUserFeed, getGlobalFeed } from '../feed/model.events';
+import { getTags } from '../tags/model';
 
 export const changeText = createEvent();
 export const signIn = createEvent();
 export const signUp = createEvent();
 export const logOut = createEvent();
+
+export const setToken = createEffect().use((token) =>
+  localStorage.setItem(TOKEN_NAME, token),
+);
+
+export const getToken = createEffect().use(() =>
+  localStorage.getItem(TOKEN_NAME),
+);
+
+export const removeToken = createEffect().use(() =>
+  localStorage.removeItem(TOKEN_NAME),
+);
 
 export const onChangeText = changeText.prepend((e) => ({
   [e.currentTarget.name]: e.currentTarget.value,
@@ -19,6 +35,14 @@ export const asyncSignUp = createEffect().use(({ name, email, password }) =>
 );
 
 export const getUser = createEffect().use(() => get('/user'));
+
+export const initAuthApp = createEffect().use(() =>
+  Promise.all([getUser(), getUserFeed(), getTags()]),
+);
+
+export const intitNotAuthApp = createEffect().use(() =>
+  Promise.all([getGlobalFeed(), getTags()]),
+);
 
 export const authDone = merge([
   asyncSignIn.done,
