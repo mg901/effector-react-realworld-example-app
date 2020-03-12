6f0a78f764cc4229920b987bf5d367a1bc8406fa
mg901 <mgreen097@gmail.com>
2020-03-12 18:55:47 +0300

    refactor(router): add readonly generic for types

--- src/modules/router/model.ts
+++ src/modules/router/model.ts
@@ -2,10 +2,10 @@ import { createEvent } from 'effector';
 import { Location, History, Action } from 'history';
 import { history } from './history';
 
-type HistoryUpdate = {
+type HistoryUpdate = Readonly<{
   location: Location<History.PoorMansUnknown>;
   action: Action;
-};
+}>;
 
 const historyUpdate = createEvent<HistoryUpdate>();
 
