73b4bfeac6692b555fa0df918c2118493bb41350
mg901 <mgreen097@gmail.com>
2020-06-06 06:24:44 +0300

    refactor(auth): add constant for token name

--- src/auth/model.ts
+++ src/auth/model.ts
@@ -1,11 +1,12 @@
 import { createEvent, createStore } from 'effector';
 import withStorage from 'effector-storage';
+import { TOKEN_NAME } from '../config';
 import { Token, AuthorizedUser, AuthFail } from './types';
 
 export const loggedOutClicked = createEvent();
 
 const createStorageStore = withStorage(createStore);
-export const $token = createStorageStore<Token>(null, { key: 'jwt' });
+export const $token = createStorageStore<Token>(null, { key: TOKEN_NAME });
 export const $$isAuth = $token.map(Boolean);
 
 export const $authorizedUser = createStore<AuthorizedUser>({
