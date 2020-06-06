e0a69742848e3a4a83fc8f56756b99db57a7363b
mg901 <mgreen097@gmail.com>
2020-06-06 03:54:09 +0300

    chore(pages): add types

--- src/pages/types.ts
+++ src/pages/types.ts
@@ -1,4 +1,37 @@
-export type Feed = {
-  articles: any[];
+export type AuthorizedUser = Readonly<{
+  image: string;
+  username: string;
+  bio: string;
+  email: string;
+  token: string | null;
+  id: number | null;
+  createdAt: string;
+  updatedAt: string;
+}>;
+
+export type Author = Readonly<{
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+}>;
+
+export type Article = Readonly<{
+  title: string;
+  slug: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  tagList: readonly string[];
+  description: string;
+  author: Author;
+  favorited: boolean;
+  favoritesCount: number;
+}>;
+
+export type Slug = Article['slug'];
+
+export type Feed = Readonly<{
+  articles: readonly Article[];
   articlesCount: number;
-};
+}>;
