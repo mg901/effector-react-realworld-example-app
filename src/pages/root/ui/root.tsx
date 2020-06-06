010e8794557bbf0a739924307b0d77551b3b1f20
mg901 <mgreen097@gmail.com>
2020-06-06 23:13:33 +0300

    chore(ui): add container

--- src/pages/root/ui/root.tsx
+++ src/pages/root/ui/root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Routes } from '../../../router';
+import { Container } from '../../../ui';
 import { RootGate } from '../model';
 import '../init';
 
@@ -8,11 +9,13 @@ export const RootPage: React.FC = () => (
   <>
     <RootGate />
     <header>
-      <Link to="/">Home</Link>
-      <nav>
-        <Link to="/login">Sign In</Link>
-        <Link to="/registration">Sign Up</Link>
-      </nav>
+      <Container>
+        <Link to="/">Home</Link>
+        <nav>
+          <Link to="/login">Sign In</Link>
+          <Link to="/registration">Sign Up</Link>
+        </nav>
+      </Container>
     </header>
     <Routes />
   </>
