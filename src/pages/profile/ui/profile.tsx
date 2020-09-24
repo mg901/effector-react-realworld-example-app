ca9986d0835bc7b2807ae8267c8794795288798d
mg901 <mgreen097@gmail.com>
2020-09-24 18:12:21 +0300

    refactor(profile): add tabs component

--- src/pages/profile/ui/profile.tsx
+++ src/pages/profile/ui/profile.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { RouteConfigComponentProps, renderRoutes } from 'react-router-config';
 import { useGate } from 'effector-react';
-import { Container, Row, NavItem, NavLink } from 'ui';
+import { Container, Row } from 'ui';
 import { routes } from '../library';
 import { model } from '../model';
+import { Tabs } from './tabs';
 import { UserInfo } from './user-info';
 import '../model/init';
 
@@ -17,18 +18,9 @@ export const ProfilePage: React.FC<Props> = ({ match: { url } }) => {
       <UserInfo />
       <Container>
         <Row>
-          <div className="col-xs-12 col-md-10 offset-md-1">
-            <ul className="nav nav-pills outline-active articles-toggle">
-              <NavItem>
-                <NavLink to={`${url}`}>My Articles</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to={`${url}/favorites`}>Favorited Articles</NavLink>
-              </NavItem>
-            </ul>
-
+          <Tabs path={url}>
             <div>{renderRoutes(routes)}</div>
-          </div>
+          </Tabs>
         </Row>
       </Container>
     </div>
