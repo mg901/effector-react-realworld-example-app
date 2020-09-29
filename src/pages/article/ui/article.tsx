cf43ef8bde0fce2a689dcfc496c5446a7b8adce5
mg901 <mgreen097@gmail.com>
2020-09-29 17:10:46 +0300

    chore: minor edits

--- src/pages/article/ui/article.tsx
+++ src/pages/article/ui/article.tsx
@@ -12,38 +12,43 @@ import '../model/init';
 export const ArticlePage: React.FC = () => {
   useGate(model.PageGate);
   const { title, author, createdAt, body, tagList } = useStore(model.$article);
+  const loading = useStore(model.getArticleFx.pending);
 
   const markup = {
     __html: marked(body),
   };
 
   return (
-    <div className="article-page">
-      <Banner>
-        <Container>
-          <h1>{title}</h1>
-          <ArticleMeta author={author} createdAt={createdAt}>
-            <EditMode />
-          </ArticleMeta>
-        </Container>
-      </Banner>
-      <Page>
-        <Row className="article-content">
-          <div className="col-xs-12">
-            <div dangerouslySetInnerHTML={markup} />
-            <TagList>
-              {tagList.map((tag) => (
-                <Tag key={tag}>{tag.toLowerCase()}</Tag>
-              ))}
-            </TagList>
-          </div>
-        </Row>
-        <hr />
-        <div className="article-actions" />
-        <Row>
-          <Comments />
-        </Row>
-      </Page>
-    </div>
+    <>
+      {!loading && (
+        <div className="article-page">
+          <Banner>
+            <Container>
+              <h1>{title}</h1>
+              <ArticleMeta author={author} createdAt={createdAt}>
+                <EditMode />
+              </ArticleMeta>
+            </Container>
+          </Banner>
+          <Page>
+            <Row className="article-content">
+              <div className="col-xs-12">
+                <div dangerouslySetInnerHTML={markup} />
+                <TagList>
+                  {tagList.map((tag) => (
+                    <Tag key={tag}>{tag.toLowerCase()}</Tag>
+                  ))}
+                </TagList>
+              </div>
+            </Row>
+            <hr />
+            <div className="article-actions" />
+            <Row>
+              <Comments />
+            </Row>
+          </Page>
+        </div>
+      )}
+    </>
   );
 };
