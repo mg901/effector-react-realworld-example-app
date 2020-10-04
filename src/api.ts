2e30d4dfb06e3229c6ce8e4400bc5ee66f52c2bd
mg901 <mgreen097@gmail.com>
2020-10-04 12:38:14 +0300

    refactor(api): add a check for a token

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,8 @@ export const request = axios.create({
   baseURL: 'https://conduit.productionready.io/api/',
 });
 
-model.$token.updates.watch((token) => {
-  request.defaults.headers.common.Authorization = `Token ${token ?? ''}`;
+model.$token.watch((token) => {
+  if (token) {
+    request.defaults.headers.common.Authorization = `Token ${token}`;
+  }
 });
