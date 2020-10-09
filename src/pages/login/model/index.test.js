461921b52ac96bc78e8b5ff63cba94d5214a026a
mg901 <mgreen097@gmail.com>
2020-10-09 16:19:18 +0300

    test: remove deprecated scripts

--- src/pages/login/model/index.test.js
+++ src/pages/login/model/index.test.js
@@ -1,5 +1,5 @@
 describe('pages/login', () => {
   it('return correct login', () => {
-    expect(process.env.USER_LOGIN).toEqual(process.env.USER_LOGIN);
+    expect(process.env.USER_LOGIN).toEqual('102928@1029.com');
   });
 });
