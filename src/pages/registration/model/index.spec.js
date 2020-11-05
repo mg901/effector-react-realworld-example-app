f35ec43e7496ecea5dd92481005be546caa6009d
mg901 <mgreen097@gmail.com>
2020-11-05 16:41:21 +0300

    test: add test for rejected signUpFx

--- src/pages/registration/model/index.spec.js
+++ src/pages/registration/model/index.spec.js
@@ -1,6 +1,6 @@
 import { root, fork, allSettled } from 'effector-root';
 import { $user, $token, $isAuthorized } from 'shared-modules/user/model';
-import { signUpFx } from './index';
+import { signUpFx, $errors } from './index';
 
 describe('pages/registration: ', () => {
   it('should successfully register via username, email and password', async () => {
@@ -28,5 +28,19 @@ describe('pages/registration: ', () => {
     expect(scope.getState($user)).toMatchObject(expected);
     expect(scope.getState($token)).toBe(expected.token);
     expect(scope.getState($isAuthorized)).toBeTruthy();
+  });
+
+  it('should return a message in case of unsuccessful registration', async () => {
+    const expected = {
+      errors: {
+        'email or password': ['is invalid'],
+      },
+    };
+
+    signUpFx.use(() => Promise.reject(expected));
+
+    const scope = fork(root);
+    await allSettled(signUpFx, { scope });
+    expect(scope.getState($errors)).toMatchObject(expected);
   });
 });
