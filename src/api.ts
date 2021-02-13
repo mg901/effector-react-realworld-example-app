4c8437c090096a0cc0f24bfee02bed4e980584a2
mg901 <mgreen097@gmail.com>
2021-02-14 01:58:15 +0300

    refactor(api): remove router import

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
-import * as router from './library/router';
 
 export const api = axios.create({
   baseURL: process.env.API_ROOT || 'https://conduit.productionready.io/api/',
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      router.model.history.push(router.Paths.LOGIN);
-    }
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       router.model.history.push(router.Paths.LOGIN);
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
 
 export const setToken = (token: null | string): void => {
   if (token) {
