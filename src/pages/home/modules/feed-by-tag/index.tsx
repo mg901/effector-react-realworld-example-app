d44a8b63eca8e4e55ed1b825403793d2c8409286
mg901 <mgreen097@gmail.com>
2021-04-02 00:37:04 +0300

    refactor(use-list): add getKey function

--- src/pages/home/modules/feed-by-tag/index.tsx
+++ src/pages/home/modules/feed-by-tag/index.tsx
@@ -25,16 +25,19 @@ export const FeedByTagPage: React.FC = () => {
     <>
       <EmptyArticles show={isEmptyFeed} />
       <ArticlesWrapper>
-        {useList(model.$articles, (article) => (
-          <li>
-            <ArticlePreview
-              data={article}
-              onClick={() => {
-                model.favoriteToggled(article);
-              }}
-            />
-          </li>
-        ))}
+        {useList(model.$articles, {
+          getKey: (item) => item.slug,
+          fn: (item) => (
+            <li>
+              <ArticlePreview
+                data={item}
+                onClick={() => {
+                  model.favoriteToggled(item);
+                }}
+              />
+            </li>
+          ),
+        })}
       </ArticlesWrapper>
       <Pagination
         current={currentPage}
