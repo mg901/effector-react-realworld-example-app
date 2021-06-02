46e1057aea405eba522980f85af4bb025d2c52cf
mg901 <mgreen097@gmail.com>
2021-06-03 01:33:43 +0300

    build(webpack): replace webpack directory with .webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { merge } = require('webpack-merge');
-const common = require('./webpack/common.config.js');
-const prod = require('./webpack/production.config.js');
-const dev = require('./webpack/development.config.js');
+const common = require('./.webpack/common.config.js');
+const prod = require('./.webpack/production.config.js');
+const dev = require('./.webpack/development.config.js');
 
 const IS_DEVELOPMENT =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
