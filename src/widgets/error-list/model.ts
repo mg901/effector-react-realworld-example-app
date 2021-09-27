da5f7e15b4c720f049135890fa1c25a621a0555b
mg901 <mgreen097@gmail.com>
2021-09-27 14:42:46 +0300

    test(jest): add jest-environment-jsdom

--- src/widgets/error-list/model.ts
+++ src/widgets/error-list/model.ts
@@ -9,8 +9,8 @@ export const $errors = createStore<Errors>({
   errors: {},
 });
 
-export const $hasErrors = $errors.map((x) => Object.keys(x).length > 0);
-export const $errorsList = $errors.map((x) => Object.entries(x.errors));
+export const $hasErrors = $errors.map((x) => Object.keys(Object(x)).length > 0);
+export const $errorsList = $errors.map((x) => Object.entries(Object(x.errors)));
 
 export const selectors = {
   useHasErrors: (): boolean => useStore($hasErrors),
