de436b012293bcee8a0084ec2f0c6bd091df16ba
Maxim <mgreen097@gmail.com>
2021-11-25 22:47:49 +0300

    refactor: remove the props that are not needed by components (#1070)

--- src/pages/article/comments/ui/comment.tsx
+++ src/pages/article/comments/ui/comment.tsx
@@ -1,48 +1,15 @@
-import { useParams, Link } from 'shared/library/router';
-import { types, model } from '../model';
-import { ButtonDelete } from './button-delete';
+import { types } from '../model';
+import { CommentFooterProps, Footer } from './comment-footer';
 
-type Props = Readonly<{
-  data: types.Comment;
-}>;
+type Props = CommentFooterProps & Pick<types.Comment, 'body'>;
 
-export function Comment({ data }: Props): JSX.Element {
+export const Comment: React.FC<Props> = ({ body, ...props }) => {
   return (
     <div className="card">
       <div className="card-block">
-        <p className="card-text">{data.body}</p>
+        <p className="card-text">{body}</p>
       </div>
-      <Footer {...data} />
+      <Footer {...props} />
     </div>
   );
-}
-
-export function Footer({
-  author,
-  createdAt,
-  id,
-}: Readonly<types.Comment>): JSX.Element {
-  const { slug } = useParams<{ slug: string }>();
-
-  const handleDeleteComment = () => {
-    model.commentDeleted({ slug, id });
-  };
-
-  return (
-    <div className="card-footer">
-      <Link className="comment-author" to={`/@${author.username}`}>
-        <img
-          alt={author.username}
-          className="comment-author-img"
-          src={author.image}
-        />
-      </Link>
-      &nbsp;
-      <Link className="comment-author" to={`/@${author.username}`}>
-        {author.username}
-      </Link>
-      <span className="date-posted">{new Date(createdAt).toDateString()}</span>
-      <ButtonDelete author={author} onClick={handleDeleteComment} />
-    </div>
-  );
-}
+};
