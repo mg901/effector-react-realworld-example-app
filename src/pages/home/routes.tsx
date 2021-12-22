26aed40a0ebb24f1c969d801a674b31e14f82a5b
Maxim <mgreen097@gmail.com>
2021-12-23 02:58:05 +0300

    refactor: add code splitting for pages (#1141)

--- src/pages/home/routes.tsx
+++ src/pages/home/routes.tsx
@@ -1,37 +1,40 @@
+import { lazy } from 'react';
 import { Switch, Redirect, Route, useRouteMatch } from 'react-router-dom';
 import * as visitor from 'entities/visitor';
 import NoMatchPage from 'pages/not-match';
-import { URLS, PrivateRoute } from 'shared/library/router';
+import { URLS, RoutesWrapper, PrivateRoute } from 'shared/library/router';
 
-import FeedByTagPage from './pages/feed-by-tag';
-import GlobalFeedPage from './pages/global-feed';
-import YourFeedPage from './pages/your-feed';
+const GlobalFeed = lazy(() => import('./pages/global-feed'));
+const YourFeed = lazy(() => import('./pages/global-feed'));
+const FeedByTag = lazy(() => import('./pages/feed-by-tag'));
 
 export const Routes: React.FC = () => {
   const { path } = useRouteMatch<{ path: string }>();
   const isAuth = visitor.selectors.useIsAuthorized();
 
   return (
-    <Switch>
-      <Route exact path="/home">
-        {isAuth ? (
-          <Redirect to={`${path}${URLS.YOUR_FEED}`} />
-        ) : (
-          <Redirect to={`${path}${URLS.GLOBAL_FEED}`} />
-        )}
-      </Route>
-      <Route path={`${path}${URLS.GLOBAL_FEED}`}>
-        <GlobalFeedPage />
-      </Route>
-      <PrivateRoute path={`${path}${URLS.YOUR_FEED}`}>
-        <YourFeedPage />
-      </PrivateRoute>
-      <Route path={`${path}${URLS.FEED_BY_TAG}`}>
-        <FeedByTagPage />
-      </Route>
-      <Route path="*">
-        <NoMatchPage />
-      </Route>
-    </Switch>
+    <RoutesWrapper>
+      <Switch>
+        <Route exact path="/home">
+          {isAuth ? (
+            <Redirect to={`${path}${URLS.YOUR_FEED}`} />
+          ) : (
+            <Redirect to={`${path}${URLS.GLOBAL_FEED}`} />
+          )}
+        </Route>
+        <Route path={`${path}${URLS.GLOBAL_FEED}`}>
+          <GlobalFeed />
+        </Route>
+        <PrivateRoute path={`${path}${URLS.YOUR_FEED}`}>
+          <YourFeed />
+        </PrivateRoute>
+        <Route path={`${path}${URLS.FEED_BY_TAG}`}>
+          <FeedByTag />
+        </Route>
+        <Route path="*">
+          <NoMatchPage />
+        </Route>
+      </Switch>
+    </RoutesWrapper>
   );
 };
--- src/pages/profile/routes.tsx
+++ src/pages/profile/routes.tsx
@@ -1,24 +1,27 @@
+import { lazy } from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import NoMatch from 'pages/not-match';
-import { PrivateRoute } from 'shared/library/router';
+import { RoutesWrapper, PrivateRoute } from 'shared/library/router';
 
-import FavoritedArticles from './pages/favorited-articles';
-import MyArticles from './pages/my-articles';
+const MyArticles = lazy(() => import('./pages/my-articles'));
+const FavoritedArticles = lazy(() => import('./pages/favorited-articles'));
 
 export const Routes: React.FC = () => {
   const { path } = useRouteMatch<{ path: string }>();
 
   return (
-    <Switch>
-      <PrivateRoute exact path={path}>
-        <MyArticles />
-      </PrivateRoute>
-      <PrivateRoute path={`${path}/favorites`}>
-        <FavoritedArticles />
-      </PrivateRoute>
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </Switch>
+    <RoutesWrapper>
+      <Switch>
+        <PrivateRoute exact path={path}>
+          <MyArticles />
+        </PrivateRoute>
+        <PrivateRoute path={`${path}/favorites`}>
+          <FavoritedArticles />
+        </PrivateRoute>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+    </RoutesWrapper>
   );
 };
