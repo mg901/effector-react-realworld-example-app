c100e25b36dfb6234810dcc731fb130cda797ae7
mg901 <mgreen097@gmail.com>
2021-12-23 14:56:40 +0300

    build(deps): add regenerator-runtime

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        useBuiltIns: 'entry',
+        useBuiltIns: 'usage',
         corejs: 3,
       },
     ],
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { App } from './app';
