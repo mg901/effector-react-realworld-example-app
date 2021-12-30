8c4ef6145488d7cfcd89a1fda720142f00ec428e
mg901 <mgreen097@gmail.com>
2021-12-31 02:59:52 +0300

    refactor: remove default-page component

--- src/pages/home/routes.tsx
+++ src/pages/home/routes.tsx
@@ -1,21 +1,27 @@
 import { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import * as visitor from '@/entities/visitor';
 
 import NoMatchPage from '@/pages/no-match';
 import { ROUTES, PrivateRoute } from '@/shared/router';
 import { Spinner } from '@/shared/ui';
-import { DefaultPage } from './pages/default';
 
 const GlobalFeedPage = lazy(() => import('./pages/global-feed'));
 const YourFeedPage = lazy(() => import('./pages/global-feed'));
 const FeedByTagPage = lazy(() => import('./pages/feed-by-tag'));
 
 export const Routes = () => {
+  const isAuth = visitor.selectors.useIsAuthorized();
+
   return (
     <Suspense fallback={<Spinner />}>
       <Switch>
         <Route exact path={ROUTES.ROOT}>
-          <DefaultPage />
+          {isAuth ? (
+            <Redirect to={ROUTES.YOUR_FEED} />
+          ) : (
+            <Redirect to={ROUTES.GLOBAL_FEED} />
+          )}
         </Route>
         <Route path={ROUTES.GLOBAL_FEED}>
           <GlobalFeedPage />
