7d56fdde71b4418fda988ef4883076ea2296ff9f
mg901 <mgreen097@gmail.com>
2021-12-31 12:06:04 +0300

    refactor(home): add default page

--- src/pages/home/routes.tsx
+++ src/pages/home/routes.tsx
@@ -1,27 +1,21 @@
 import { lazy, Suspense } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import * as visitor from '@/entities/visitor';
+import { Switch, Route } from 'react-router-dom';
 
 import NoMatchPage from '@/pages/no-match';
 import { ROUTES, PrivateRoute } from '@/shared/router';
 import { Spinner } from '@/shared/ui';
+import { Default } from './pages/default';
 
 const GlobalFeedPage = lazy(() => import('./pages/global-feed'));
 const YourFeedPage = lazy(() => import('./pages/global-feed'));
 const FeedByTagPage = lazy(() => import('./pages/feed-by-tag'));
 
 export const Routes = () => {
-  const isAuth = visitor.selectors.useIsAuthorized();
-
   return (
     <Suspense fallback={<Spinner />}>
       <Switch>
         <Route exact path={ROUTES.ROOT}>
-          {isAuth ? (
-            <Redirect to={ROUTES.YOUR_FEED} />
-          ) : (
-            <Redirect to={ROUTES.GLOBAL_FEED} />
-          )}
+          <Default />
         </Route>
         <Route path={ROUTES.GLOBAL_FEED}>
           <GlobalFeedPage />
