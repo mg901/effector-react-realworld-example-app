6754be42a829d9db9b6252f0f36c2428bacbbb35
Maxim <mgreen097@gmail.com>
2022-01-03 02:04:53 +0300

    refactor(router): rewrite private route (#1186)

--- src/shared/router/ui/private-route.tsx
+++ src/shared/router/ui/private-route.tsx
@@ -2,8 +2,24 @@ import { Route, Redirect, RouteProps } from 'react-router-dom';
 import * as visitor from '@/entities/visitor';
 import { ROUTES } from '../routes';
 
-export const PrivateRoute = (props: RouteProps) => {
+export const PrivateRoute = ({ children, ...props }: RouteProps) => {
   const isAuth = visitor.selectors.useIsAuthorized();
 
-  return isAuth ? <Route {...props} /> : <Redirect to={ROUTES.LOGIN} />;
+  return (
+    <Route
+      {...props}
+      render={({ location }) =>
+        isAuth ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: ROUTES.LOGIN,
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
 };
