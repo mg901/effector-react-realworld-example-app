374cfa9673caae952d76cf0058172275c1e53d76
Алешин Максим <mgreen097@gmail.com>
2022-01-27 19:57:20 +0300

    refactor: rewrite model

--- src/pages/editor/model/store.ts
+++ src/pages/editor/model/store.ts
@@ -11,16 +11,43 @@ import * as article from '@/entities/article';
 import { history, createParamsStore, ROUTES } from '@/shared/router';
 import * as api from './api';
 
+export const Gate = createGate();
+export const $slug = createParamsStore<{ slug: string }>({
+  path: ROUTES.editor.slug,
+}).map((params) => params.slug ?? '');
+
+const $hasSlug = $slug.map(Boolean);
+const $isEmptySlug = $slug.map((slug) => slug.length === 0);
+
+export const getArticleFx = createEffect(api.getArticle);
+
+guard({
+  source: $slug,
+  filter: Boolean,
+  clock: Gate.open,
+  target: getArticleFx,
+});
+
 export const formSubmitted = createEvent<article.types.Article>();
-export const tagDeleted = createEvent<string>();
+export const updateArticleFx = createEffect(api.updateArticle);
+
+sample({
+  source: $slug,
+  clock: guard(formSubmitted, { filter: $hasSlug }),
+  fn: (slug, fields) => ({ ...fields, slug }),
+  target: updateArticleFx,
+});
+
 export const createArticleFx = createEffect<
   article.types.Article,
   article.types.Article,
   Record<string, unknown>
 >(api.createArticle);
 
-export const getArticleFx = createEffect(api.getArticle);
-export const updateArticleFx = createEffect(api.updateArticle);
+sample({
+  clock: guard(formSubmitted, { filter: $isEmptySlug }),
+  target: createArticleFx,
+});
 
 export const redirectToArticleIdFx = createEffect(
   ({ slug }: article.types.Article) => {
@@ -31,34 +58,6 @@ export const redirectToArticleIdFx = createEffect(
 forward({
   from: [updateArticleFx.doneData, createArticleFx.doneData],
   to: redirectToArticleIdFx,
-});
-
-export const Gate = createGate();
-
-export const $slug = createParamsStore<{ slug: string }>({
-  path: ROUTES.editor.slug,
-}).map((params) => params.slug ?? '');
-
-const $hasSlug = $slug.map(Boolean);
-const $isEmptySlug = $slug.map((slug) => slug.length === 0);
-
-guard({
-  source: $slug,
-  filter: Boolean,
-  clock: Gate.open,
-  target: getArticleFx,
-});
-
-sample({
-  source: $slug,
-  clock: guard(formSubmitted, { filter: $hasSlug }),
-  fn: (slug, fields) => ({ ...fields, slug }),
-  target: updateArticleFx,
-});
-
-sample({
-  clock: guard(formSubmitted, { filter: $isEmptySlug }),
-  target: createArticleFx,
 });
 
 export const $error = restore(createArticleFx.failData, {
