a70049384e53deb9375a6c4b56af929d296d32fd
Алешин Максим <mgreen097@gmail.com>
2022-02-07 14:22:40 +0300

    test(comments): rename `addCommentFx` with `addFx`

--- src/entities/comment/model/store.spec.js
+++ src/entities/comment/model/store.spec.js
@@ -1,14 +1,14 @@
 import { fork, allSettled } from 'effector';
-import { addCommentFx, $error } from './store';
+import { addFx, $error } from './store';
 
 describe('pages/article/store', () => {
   it('should return an error if you submit a form with empty fields', async () => {
     const expected = { errors: { body: ["can't be blank"] } };
 
-    addCommentFx.use(() => Promise.reject(expected));
+    addFx.use(() => Promise.reject(expected));
 
     const scope = fork();
-    await allSettled(addCommentFx, { scope });
+    await allSettled(addFx, { scope });
     expect(scope.getState($error)).toMatchObject(expected);
   });
 });
