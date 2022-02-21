4250ef3dec37e207a7b98dfe1447a242bbcb6dd2
Алешин Максим <mgreen097@gmail.com>
2022-02-21 15:28:25 +0300

    test: add tests for http-client adapter

--- src/shared/http/adapter/adapter.spec.ts
+++ src/shared/http/adapter/adapter.spec.ts
@@ -6,10 +6,18 @@ import { createAdapter, HttpClientError } from './adapter';
 describe('client', () => {
   let mock: MockAdapter;
   let client: types.IHttpClient;
+  let BASE_URL: string;
+  let ACCESS_TOKEN: string;
 
   beforeEach(() => {
     mock = new MockAdapter(axios);
     client = createAdapter(axios);
+    BASE_URL = 'http://localhost:8080';
+    ACCESS_TOKEN = '123-456';
+
+    client.init({
+      baseURL: BASE_URL,
+    });
   });
 
   afterEach(() => {
@@ -17,18 +25,38 @@ describe('client', () => {
   });
 
   it('should return a response object', () => {
-    const responseData = {
-      users: [{ id: 1, name: 'John Smith' }],
+    const data = {
+      users: [
+        {
+          id: 1,
+          name: 'John Smith',
+        },
+      ],
     };
 
-    mock.onGet('/users').reply(200, responseData);
+    mock.onGet('/users').reply(200, data);
+
+    const responseMock = {
+      data,
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        Authorization: 'Token 123-456',
+      },
+      ok: true,
+      status: 200,
+      statusText: '',
+      url: `${BASE_URL}/users`,
+    };
 
     expect(
       client.request({
         url: '/users',
         method: 'GET',
+        headers: {
+          Authorization: `Token ${ACCESS_TOKEN}`,
+        },
       }),
-    ).resolves.toEqual(responseData);
+    ).resolves.toEqual(responseMock);
   });
 
   it('should throw an custom error', () => {
